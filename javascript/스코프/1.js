d6556a4066840c4a01d6c2d73632d288229ca258
qwa1822 <asdzxc1822@naver.com>
2022-11-24 17:01:35 +0900

    scope

--- javascript/스코프/1.js
+++ javascript/스코프/1.js
@@ -8,3 +8,25 @@
  */
 
 // 변수는 최대한 필요한곳에서만 
+// 식별자가 유효한 범위내에서만
+// 코드 블럭: {}, if() , for() ,{}, funcntion() {}
+// 블럭 외부에서 블럭 내부의 참조불가
+
+{
+    const a='a';
+    console.log(a);
+}
+
+// 접근불가
+console.log(a);
+const b='b';
+
+function print(){
+    const message="message";
+    console.log(message);
+}
+
+console.log(message);
+
+// g함수 외부에서는 함수의 매개변수를 참조x
+
--- javascript/스코프/2.js
+++ javascript/스코프/2.js
@@ -1,9 +1,7 @@
+const text='global'; //전역변수
 {
-    const x=1
-{
-    const y=2;
-    console.log(x);
-}
-console.log(x);
-console.log(y);
+    const text="insdie block1"
+    {
+        const text='inside block2'
+    }
 }
